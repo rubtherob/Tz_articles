80960634ff2cb0ad4c63d460cca470340655d5fe
Dima <rubtherobs@gmail.com>
2022-04-22 15:58:50 +0300

    commit

--- TZ/settings.py
+++ TZ/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,12 +84,10 @@ DATABASES = {
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 
-    'default': {
-        'NAME': 'TZ',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'postgres',
+
+    'default': dj_database_url.config()
 }
-}
+
 
 
 # Password validation
@@ -141,3 +139,4 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'users.User'
+
